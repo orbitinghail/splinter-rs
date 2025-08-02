94e535e0a2f6b8e9eb72abc0e43f4c31d90fac95
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-08-01 22:21:35 -0700

    cargo clippy

--- src/splinterv2/encode.rs
+++ src/splinterv2/encode.rs
@@ -1,6 +1,6 @@
 //! TODO: Encoder/Encodable
 //! The idea is to build a serde style encoder which can be abstracted over
-//! various destination types, while making heavy use of bytes::bufmut and zerocopy.
+//! various destination types, while making heavy use of `bytes::bufmut` and zerocopy.
 //!
 //! The main benefit of Splinter's tail encoding is that the offset indexes are
 //! emitted after each partition. This allows us to efficiently seek through the
@@ -8,7 +8,7 @@
 //!
 //! However, we could still put offset indexes in the beginning by reserving
 //! space ahead of time. Since cardinality is no longer expensive to compute, we
-//! should be able to easily do this with the BufMut API.
+//! should be able to easily do this with the `BufMut` API.
 //!
 //! The other benefit of tail encoding is that we can encode directly into a
 //! socket/stream. We aren't currently taking advantage of this.
