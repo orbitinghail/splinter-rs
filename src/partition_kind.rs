1359783ce3738a9912e64cb9a1752953a7f7bb76
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-08-28 11:09:03 -0700

    fix cargo clippy

--- src/partition_kind.rs
+++ src/partition_kind.rs
@@ -4,18 +4,18 @@ use crate::partition::Partition;
 
 use crate::level::Level;
 
-/// PartitionKind is a one byte bitfield which currently only uses the first
+/// `PartitionKind` is a one byte bitfield which currently only uses the first
 /// three bits (LE). The remaining bits are reserved for future expansion.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default, TryFromBytes)]
 #[repr(u8)]
 pub enum PartitionKind {
     #[default]
-    Empty = 0b00000_000,
-    Full = 0b00000_001,
-    Bitmap = 0b00000_010,
-    Vec = 0b00000_011,
-    Run = 0b00000_100,
-    Tree = 0b00000_101,
+    Empty = 0b000,
+    Full = 0b001,
+    Bitmap = 0b010,
+    Vec = 0b011,
+    Run = 0b100,
+    Tree = 0b101,
 }
 
 impl PartitionKind {
